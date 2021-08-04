b8ae09e94754f41cf3ddae5d0ea591889cea6671
Trishtan <trishtan.pamatmat@gmail.com>
2021-08-04 07:09:43 +0000

    Activity 2

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example_module
 
 go 1.16
+
+require github.com/go-playground/assert v1.2.1 // indirect
--- src/main.go
+++ src/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main(){
-  println("Hello, world!")
+import (
+	"fmt"
+
+	"example_module/src/greetings"
+)
+
+func main() {
+	fmt.Printf("%s, world!\n", greetings.Greeting())
 }
